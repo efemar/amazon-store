77827ce8b47a62903c9a11fb10461a336d20603e
Carmen SanDiego <efemar82@gmail.com>
2019-09-29 18:00:38 -0700

    pseudocode

---
+++ bamazonCustomer.js
@@ -0,0 +1,11 @@
+// Create a connection with SQL
+// Display all of the products in the table
+
+//Using inquirer, ask the user the following:
+//The first should ask them the ID of the product they would like to buy.
+//The second message should ask how many units of the product they would like to buy.
+
+//Check if store has enough quantities of the item.
+//if not, Console log ("Insufficient quantity")
+//if yes,  fullfill the customer's order and update the table
+//Show customer the total purchase price
