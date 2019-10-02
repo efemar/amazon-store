7687d99eb28c77b4960d0bd78885e3af6e5da76e
Carmen SanDiego <efemar82@gmail.com>
2019-10-01 20:39:54 -0700

    added a few functions to clean up code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,71 +47,74 @@ function initialize() {
 
             }
         ]).then(function (answers) {
-            var product = answers.id;
-            var query = "SELECT * FROM products WHERE item_id=" + product + "";
-
-            if (product === "Q") {
-                console.log("Goodbye");
-                connection.end();
-                return;
-
+    
+           if (answers.id === "Q") {
+                quit();
+                return;     
             } else {
-                connection.query(query, function (err, res) {
-                    if (err) throw err;
-                    if (res.length < 1) {
-                        console.log("That item is not in the inventory");
-                        initialize();
-                    }
-                    else {
-                        inquirer.prompt([
-                            {
-                                name: "quantity",
-                                type: "input",
-                                message: "How many would you like? [Quit with Q]",
-
-                            }
-                        ]).then(function (answers) {
-                            var quantity = answers.quantity;
-                            if (quantity === "Q") {
-                                console.log("Goodbye");
-                                connection.end();
-                                return;
-                            }
-                            
-                            else if (quantity <= res[0].stock_quantity)   {
-                                console.log("Successfully purchased " + quantity + " " + res[0].product_name + "(s)");
-                                console.log("Your total price is: " + (quantity * res[0].price) + " dollars")
-                                //Need to update table with the new inventory
-                                newQuantity = res[0].stock_quantity - quantity;
-                                connection.query("UPDATE products SET ? WHERE ?",
-                                [
-                                  {
-                                    stock_quantity: newQuantity
-                                  },
-                                  {
-                                    item_id: product
-                                  }
-                                ], function (err, res) {
-                                    if (err) throw err;
-                                    initialize();
-                                })
- 
-                            }
-                            else if (quantity >= res[0].stock_quantity) {
-                                console.log("Insufficient quantity!");
-                                initialize();
-                            }
-                              
-                        })
-
-                    }
-                })
-
-            }
+                promptUserQuantity(answers.id);
+             }
         })
     })
-
 };
 
 
+function quit() {
+    console.log("Goodbye");
+    connection.end();
+}
 
+
+function promptUserQuantity(product) { 
+var query = "SELECT * FROM products WHERE item_id=" + product + "";
+connection.query(query, function (err, res) {
+    if (err) throw err;
+    if (res.length < 1) {
+        console.log("That item is not in the inventory");
+        initialize();
+    }
+    else {
+        inquirer.prompt([
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like? [Quit with Q]",
+
+            }
+        ]).then(function (answers) {
+            var quantity = answers.quantity;
+            if (quantity === "Q") {
+                quit();
+                return;
+            }
+
+            else if (quantity <= res[0].stock_quantity) {
+                console.log("Successfully purchased " + quantity + " " + res[0].product_name + "(s)");
+                console.log("Your total price is: " + (quantity * res[0].price) + " dollars")
+                //Need to update table with the new inventory
+                newQuantity = res[0].stock_quantity - quantity;
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newQuantity
+                        },
+                        {
+                            item_id: product
+                        }
+                    ], function (err, res) {
+                        if (err) throw err;
+                        initialize();
+                    })
+
+            }
+            else if (quantity >= res[0].stock_quantity) {
+                console.log("Insufficient quantity!");
+                initialize();
+            }
+
+        })
+
+    }
+})
+
+}
