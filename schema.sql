675d002d65e6a74b7d576ea3fe08fd17aa937bcc
Carmen SanDiego <efemar82@gmail.com>
2019-10-02 14:35:20 -0700

    added schema.sql file

--- schema.sql
+++ schema.sql
@@ -10,18 +10,3 @@ CREATE TABLE products (
   stock_quantity INTEGER(6),
   PRIMARY KEY (item_id)
   );
-
-USE bamazon;
-
-INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES 
-(1, "Coconut Oil", "hair", 11.99, 100),
-(2, "Hiking bag", "sports", 21.07, 21),
-(3, "Roller Blades", "sports", 112.00, 3),
-(4, "Golden Necklace", "jewlery", 37.56, 40),
-(5, "Engagement ring", "jewlery", 4000, 5),
-(6, "Printer", "computer", 158.00, 25),
-(7, "Black Levis", "clothes", 189.00, 45),
-(8, "Colombian Coffee", "food", 7.44, 67),
-(9, "Sriracha", "food", 3.56, 120),
-(10, "Dinning Table", "furniture", 256.00, 89);
